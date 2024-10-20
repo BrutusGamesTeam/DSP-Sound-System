3fef762f50683e94c4fc55f9152426b1bed217d8
BrutusGamesTeam <oficiallazarentos@gmail.com>
2024-10-20 15:43:25 -0300

    Create DSReflector.java

---
+++ scripts/DSReflector.java
@@ -0,0 +1,28 @@
+// Code Package
+package JAVARuntime;
+
+// Explicits Imports
+import JAVARuntime.*;
+
+// Dynamic Sound Reflector
+public class SoundReflector extends Component
+{
+	// Reflector Attributes
+	public float reflectFactor = 1.22f;
+	public float noiseLoss = 0.25f;
+	public float occlusion = 0.1f;
+	
+	@Override
+	// Run only Once
+	public void start()
+	{
+		
+	}
+
+	@Override
+	// Repeat every Frame
+	public void repeat()
+	{
+		
+	}
+}
